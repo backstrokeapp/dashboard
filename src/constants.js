be1c610ee9c47a6ea178deed7167c85c6415daf6
Ryan Gaus <rsg1egoman@gmail.com>
2017-09-20 07:09:48 -0400

    chore: change default url of service to be the new api

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export let API_URL;
-export const ROOT_URL = process.env.REACT_ROOT_API_URL || 'https://backstroke.us';
+export const ROOT_URL = process.env.REACT_ROOT_API_URL || 'https://api.backstroke.co';
 
 export function setApiUrl(url) {
   API_URL = url;
