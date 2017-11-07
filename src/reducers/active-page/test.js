be121d7120faf3e0376b69fe5322cce12bb8cbfb
Ryan Gaus <rsg1egoman@gmail.com>
2017-11-07 07:36:12 -0500

    chore(test): complete rpeviously empty reducer tests

--- src/reducers/active-page/test.js
+++ src/reducers/active-page/test.js
@@ -1,7 +1,48 @@
-
 import assert from 'assert';
 import activePage from './index';
 
-describe('active-page', function() {
-});
+import routeTransitionLinkDetail from '../../actions/route-transition/link-detail';
+import routeTransitionLinkList from '../../actions/route-transition/link-list';
+import routeTransitionLoginConfirmation from '../../actions/route-transition/login-confirmation';
 
+describe('active-page', function() {
+  it('should switch the active page to link list', function() {
+    // Initialize the route transition thunk.
+    const thunk = routeTransitionLinkList();
+
+    // Apply the each action the thunk emits to the reducer.
+    let response = undefined;
+    thunk(action => {
+      response = activePage(response, action)
+    });
+
+    // Assert the final value.
+    assert.equal(response, 'link-list');
+  });
+  it('should switch the active page to link detail', function() {
+    // Initialize the route transition thunk.
+    const thunk = routeTransitionLinkDetail();
+
+    // Apply the each action the thunk emits to the reducer.
+    let response = undefined;
+    thunk(action => {
+      response = activePage(response, action)
+    });
+
+    // Assert the final value.
+    assert.equal(response, 'link-detail');
+  });
+  it('should switch the active page to login confirmation', function() {
+    // Initialize the route transition thunk.
+    const thunk = routeTransitionLoginConfirmation();
+
+    // Apply the each action the thunk emits to the reducer.
+    let response = undefined;
+    thunk(action => {
+      response = activePage(response, action)
+    });
+
+    // Assert the final value.
+    assert.equal(response, 'login-confirmation');
+  });
+});
--- src/reducers/user/test.js
+++ src/reducers/user/test.js
@@ -1,7 +1,13 @@
-
 import assert from 'assert';
 import user from './index';
 
-describe('user', function() {
-});
+import userSet from '../../actions/user/set';
 
+const USER = { username: '1egoman' };
+
+describe('user', function() {
+  it('should set the content of the user', function() {
+    const response = user(undefined, userSet(USER));
+    assert.equal(response, USER);
+  });
+});
